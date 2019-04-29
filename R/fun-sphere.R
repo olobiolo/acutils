2a0c1ab04fbe31d5e44fee14b540f32f2707189d
Aleksander Chlebowski <olobiolo@gmail.com>
2019-04-29 17:26:25 +0200

    new function: sphere

---
+++ R/fun-sphere.R
@@ -0,0 +1,15 @@
+#' compute sphere volume
+#'
+#' Calculate volume of n-dimentional sphere of radius r.
+#'
+#' @param r radius
+#' @param n number of dimensions
+#'
+#' @return volume of a supersphere
+#'
+#' @export
+
+sphere <- function(r, n = 7L) {
+  if (x%%1 != 0 ) stop("no fractions allowed in \"n\"")
+  (pi^(n/2) * r^n) / gamma(1 + n/2)
+}
