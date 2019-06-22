f705a3600c2ba6ead6ed0333b505c6f9ee60fecf
Aleksander Chlebowski <olobiolo@gmail.com>
2019-06-22 15:19:44 +0200

    bug fix in function: rescale

--- R/fun-rescale.R
+++ R/fun-rescale.R
@@ -1,44 +1,38 @@
 #' scale numeric vector
 #'
-#' Scales a vector. Positive values are scaled from 0 to 1, negative ones are scaled -1 to 0.
+#' Scales a vector to a requested range.
 #'
 #' @param x a numeric vector or matrix
+#' @param range range which to scale to:
+#'              \code{positive}: [0,1], \code{negative}: [-1,0], \code{both}: [-1,1]
 #'
 #' @return a numeric object of the same class as \code{x}
 #'
 #' @export
 #'
-rescale <- function(x) {
+rescale <- function(x, range) {
   UseMethod('rescale')
 }
 
 #' @export
 #' @describeIn rescale defult method for vectors
-rescale.default <- function(x) {
+rescale.default <- function(x, range) {
   if (!is.numeric(x)) stop('"x" must be numeric')
+  #range <- match.arg('range', choices = c('positive', 'negative', 'both'), several.ok = FALSE)
 
-  s.pos <- function(x)
-    suppressWarnings((x - min(x, na.rm = T)) / (max(x, na.rm = T) - min(x, na.rm = T)))
-  s.neg <- function(x)
-    suppressWarnings((x - max(x, na.rm = T)) / (max(x, na.rm = T) - min(x, na.rm = T)))
-  pos <- x[x > 0]
-  neg <- x[x < 0]
-  zer <- x[x == 0]
-  pos.s <- s.pos(pos)
-  neg.s <- s.neg(neg)
-
-  X <- numeric(length(x))
-  X[x > 0] <- pos.s
-  X[x < 0] <- neg.s
-  X[x == 0] <- 0
-  return(X)
+  X <- (x - min(x)) / (max(x - min(x)))
+  Y <- switch(range,
+              'positive' = X,
+              'negative' = X - 1,
+              'both' = 2 * (X - 0.5))
+  return(Y)
 }
 
 #' @export
 #' @describeIn rescale calls default method and copies the
 #'             \code{dim} attribute of \code{x} to result
-rescale.matrix <- function(x) {
-  X <- rescale.default(x)
+rescale.matrix <- function(x, range) {
+  X <- rescale.default(x, range = range)
   dim(X) <- dim(x)
   return(X)
 }
