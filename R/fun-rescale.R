73d45e984c24c1d2c502196ce3165d8800992b4d
olobiolo <olobiolo@gmail.com>
2019-06-24 12:43:35 +0200

    update function: rescale

--- R/fun-rescale.R
+++ R/fun-rescale.R
@@ -2,23 +2,32 @@
 #'
 #' Scales a vector to a requested range.
 #'
+#' Values in the vector are scaled such to fill \code{range}:
+#' \itemize{
+#'   \item{\code{positive}: from 0 to 1
+#'   }
+#'   \item{\code{negative}: from -1 to 0
+#'   }
+#'   \item{\code{both}: from -1 to 1
+#'   }
+#' }
+#'
 #' @param x a numeric vector or matrix
-#' @param range range which to scale to:
-#'              \code{positive}: [0,1], \code{negative}: [-1,0], \code{both}: [-1,1]
+#' @param range range which to scale to
 #'
 #' @return a numeric object of the same class as \code{x}
 #'
 #' @export
 #'
-rescale <- function(x, range) {
+rescale <- function(x, range = c('positive', 'negative', 'both')) {
   UseMethod('rescale')
 }
 
 #' @export
 #' @describeIn rescale defult method for vectors
-rescale.default <- function(x, range) {
+rescale.default <- function(x, range = c('positive', 'negative', 'both')) {
   if (!is.numeric(x)) stop('"x" must be numeric')
-  #range <- match.arg('range', choices = c('positive', 'negative', 'both'), several.ok = FALSE)
+  range <- match.arg(range, choices = c('positive', 'negative', 'both'), several.ok = FALSE)
 
   X <- (x - min(x)) / (max(x - min(x)))
   Y <- switch(range,
@@ -31,7 +40,7 @@ rescale.default <- function(x, range) {
 #' @export
 #' @describeIn rescale calls default method and copies the
 #'             \code{dim} attribute of \code{x} to result
-rescale.matrix <- function(x, range) {
+rescale.matrix <- function(x, range = c('positive', 'negative', 'both')) {
   X <- rescale.default(x, range = range)
   dim(X) <- dim(x)
   return(X)
