a2ccd93fa60fea30135fc9d16c8004715809ee5b
olobiolo <olobiolo@gmail.com>
2019-10-25 19:34:18 +0200

    update to function retry
    now takes call rather than function
    previously coulnd not handle functions with formals
    
    function operator temporarily recalled

--- R/fun-retry.R
+++ R/fun-retry.R
@@ -1,34 +1,49 @@
-#' rerun a function in case of error
+#' rerun a function call in case of error
 #'
-#' If a function returns an error, run it again.
+#' If a call returns an error, run it again.
 #'
 #' Iterative calling of a function that can return a random error
 #' can crash a process or at the very least result in incomplete data.
 #' To alleviate the impact of random errors the failed call will be rerun
 #' until it succeeds or until a limit of attempts is reached.
 #' If all attempts fail, a backup value will be returned.
-#' Note that the iteration counter is reset only when \code{retryFO} is called
-#' and the iteration count will stack across multiple calls of modified \code{foo}.
-#' Conversely, \code{retry} runs resets the counter before it runs \code{foo}.
 #'
-#' @param foo a function
+# Note that the iteration counter is reset only when \code{retryFO} is called
+# and the iteration count will stack across multiple calls of modified \code{foo}.
+# Conversely, \code{retry} runs resets the counter before it first evaluates \code{call}.
+#'
+#' @param call call to retry
 #' @param max.iter maximum number of iterations after which failure is declared
 #' @param fail object to return in case of failure
 #' @param verbose logical flag whether to print iterations as messages
 #'
-#' @return \code{retry} runs \code{foo} and returns the same as \code{foo}
+#' @return \code{retry} returns the same as the function in \code{call}
 #' or as defined by \code{fail}.
 #'
-#' \code{retryFO} returns
-#' a modified version of \code{foo} reruns itself.
+# \code{retryFO} returns a modified version of \code{foo} that reruns itself.
 #'
 #' @export
 #'
-#' @describeIn retry
-#' runs \code{foo} untill succeeds or limit reached
+#' @examples
+#' f <- function() {
+#'   x <- sample(1:2, size = 1)
+#'   if (x == 1) stop("it's a 1", call. = TRUE)
+#'   return(x)
+#' }
+#' \dontrun{
+#' f()
+#' replicate(10, f())
+#' }
 #'
-retry <- function(foo, max.iter, fail = NA, verbose = FALSE) {
-  if (!is.function(foo)) stop('"foo" must be a function')
+#' retry(f(), 2, "failed", TRUE)
+#' table(replicate(100, retry(f(), 2, "failed", FALSE)))
+#'
+# @describeIn retry
+# attepmts to evaluate \code{call} untill it succeeds or a limit is reached
+#'
+retry <- function(call, max.iter, fail = NA, verbose = FALSE) {
+  C <- substitute(call)
+  if (!is.call(C)) stop('"call" must be a function call')
   if (!is.numeric(max.iter)) stop('"max.iter" must be numeric')
   if (max.iter > 1000) warning('a large number of attempts has been requested')
 
@@ -37,8 +52,8 @@ retry <- function(foo, max.iter, fail = NA, verbose = FALSE) {
     iter <- 0
     g <- function() {
       iter <<- iter +1
-      if (verbose) message('iteration', iter)
-      tryCatch(foo(), error = function(e) {if (iter < max.iter) g() else (return(fail))})
+      if (verbose) message('iteration ', iter)
+      tryCatch(eval(C), error = function(e) {if (iter < max.iter) g() else (return(fail))})
     }
     return(g)
   }
@@ -48,50 +63,38 @@ retry <- function(foo, max.iter, fail = NA, verbose = FALSE) {
   h()
 }
 
-#' @describeIn retry
-#' returns modified \code{foo} that retries itself untill succeeds ro limit reached
-#'
-retryFO <- function(foo, max.iter, fail = NA, verbose = FALSE) {
-  if (!is.function(foo)) stop('"foo" must be a function')
-  if (!is.numeric(max.iter)) stop('"max.iter" must be numeric')
-  if (max.iter > 1000) warning('a large number of attempts has been requested')
+# #' @param foo a function
+# #' @describeIn retry
+# #' returns modified \code{foo} that retries itself untill it succeeds or a limit reached
+# #'
+# retryFO <- function(foo, max.iter, fail = NA, verbose = FALSE) {
+#   if (!is.function(foo)) stop('"foo" must be a function')
+#   if (!is.numeric(max.iter)) stop('"max.iter" must be numeric')
+#   if (max.iter > 1000) warning('a large number of attempts has been requested')
+#
+#   # define constructor for counting runner
+#   G <- function() {
+#     iter <- 0
+#     g <- function() {
+#       iter <<- iter +1
+#       if (verbose) message('iteration', iter)
+#       tryCatch(foo(), error = function(e) {if (iter < max.iter) g() else (return(fail))})
+#     }
+#     return(g)
+#   }
+#   # run constructor to create counting runner
+#   h <- G()
+#   # return counting runner
+#   return(h)
+# }
 
-  # define constructor for counting runner
-  G <- function() {
-    iter <- 0
-    g <- function() {
-      iter <<- iter +1
-      if (verbose) message('iteration', iter)
-      tryCatch(foo(), error = function(e) {if (iter < max.iter) g() else (return(fail))})
-    }
-    return(g)
-  }
-  # run constructor to create counting runner
-  h <- G()
-  # return counting runner
-  return(h)
-}
-
-#' @examples
-#' f <- function() {
-#'   x <- sample(x = 1:2, size = 1)
-#'   if (x == 1) stop("it's a 1", call. = TRUE)
-#'   return(x)
-#' }
-#' \dontrun{
-#' f()
-#' replicate(10, f())
-#' }
-#'
-#' retry(f, 2, "failed", TRUE)
-#' table(replicate(10, retry(f, 2, "failed", FALSE)))
-#'
-#' \dontrun{
-#' f.fo <- retryFO(f, 2, "failed", TRUE)
-#' f.fo()
-#' table(replicate(10, f.fo()))
-#' }
-#'
+# @examples continued
+# \dontrun{
+# f.fo <- retryFO(f, 2, "failed", TRUE)
+# f.fo()
+# table(replicate(10, f.fo()))
+# }
+#
 
 
 
